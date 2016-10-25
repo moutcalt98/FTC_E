b2615b534a0527d926c03b9f3cb1a230c96b8ef2
Calvin Jou <calvin@calvinjou.xyz>
2016-10-24 20:57:09 -0500

    Add files

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ExampleTeleopTankMode.java
@@ -0,0 +1,36 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+@TeleOp(name="ExampleTeleopTankMode", group="Test")
+public class ExampleTeleopTankMode extends OpMode {
+
+    DcMotor leftMotor;
+    DcMotor rightMotor;
+
+    @Override
+    public void init() {
+        //get references to the motors from the hardware map
+        leftMotor = hardwareMap.dcMotor.get("left_drive");
+        rightMotor = hardwareMap.dcMotor.get("right_drive");
+
+        //reverse the right motor
+        rightMotor.setDirection(DcMotor.Direction.REVERSE);
+    }
+
+    @Override
+    public void loop() {
+        //get the values from the gamepads
+        //note: pushing the stick all the way up returns -1,
+        // so we need to reverse the values
+        float leftY = gamepad1.left_stick_y;
+        float rightY = gamepad1.right_stick_y;
+
+        //set the power of the motors with the gamepad values
+        leftMotor.setPower(rightY);
+        rightMotor.setPower(leftY);
+    }
+}
+
